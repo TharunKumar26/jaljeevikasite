e53b96991383e94eac2e6c92ac1c94a13f9cdb53
unknown <balarajutk26@gmail.com>
2020-06-08 21:27:35 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-var bodyParser = require('body-parser')
-
-
-
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }))
- 
-
-app.use(bodyParser.json())
+const expressLayouts = require('express-ejs-layouts');
 
 require('dotenv').config();
 
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors()); // cors middleware
 app.use(express.json());
-
-app.use(express.urlencoded({ extended: false }))
-
-app.set('view engine','ejs')
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
@@ -31,16 +19,9 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 });
 
-
-app.get('/',(req,res)=>{
-
-
-})
 const productsRouter = require('./routes/products');
 
 app.use('/products', productsRouter);
-
-
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
